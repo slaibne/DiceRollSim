1f5c55d7c89bb2332acb9c67bf0d7a5aaa0d1190
slaibne <ivy.hou330@icloud.com>
2023-10-14 19:48:33 -0400

    finished for loop, random num generation

--- DiceRollSim.java
+++ DiceRollSim.java
@@ -3,9 +3,31 @@ package DiceRollSim;
 /**
  * DiceRollSim
  */
+import java.util.Scanner;
+import java.util.Random;
+
 public class DiceRollSim {
 
     public static void main(String[] args) {
+        int numDice;
+        int numSides;
+        int total = 0;
+        int i;
 
+        Scanner in = new Scanner(System.in);
+
+        Random rand = new Random();
+
+        System.out.println("Please enter the number of dice.");
+        numDice = Integer.parseInt(in.nextLine());
+
+        System.out.println("Please enter the number of sides.");
+        numSides = Integer.parseInt(in.nextLine());
+
+        for (i = 0; i < numDice; i++) {
+            total = total + rand.nextInt(1, numSides);
+        }
+
+        System.out.println("Total = " + total);
     }
 }
